bd713d691b6aeb3a26b5c9c0712deced6b215aad
ads6495 <astrange55@icloud.com>
2019-08-07 15:58:52 -0400

    basic logic to get deck and loop through

--- public/main.js
+++ public/main.js
@@ -1,7 +1,41 @@
-const main = () => {
-  if (document.querySelector('h1.hello-world')) {
-    document.querySelector('h1.hello-world').textContent = 'Hello, World!'
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable quotes */
+/* eslint-disable no-unused-expressions */
+class Card {
+  constructor(suits, values) {
+    this.suits = suits
+    this.values = values
   }
 }
+const cardValues = document.querySelector('.card-values')
+const cardSuits = document.querySelector('.card-suit')
 
-document.addEventListener('DOMContentLoaded', main)
+const suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+const values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]
+let deck = []
+
+const pairs = () => {
+  for (s = 0; s < suits.length; s++) {
+    for (v = 0; v < values.length; v++) {
+      deck.push(new Card(suits[s], values[v]))
+    }
+  }
+  console.log(deck)
+  cardSuits.textContent = suits[0].suits
+  cardValues.textContent = suits[0].values
+}
+
+const shuffleDeck = () => {
+  let randomCard = deck[math.floor(Math.random() * deck.length)]
+  let firstCard = deck[0]
+  firstCard = randomCard
+  cardSuits.textContent = firstCard.suit
+  cardValues.textContent = firstCard.values
+  console.log(firstCard)
+}
+
+
+
+
+
+document.querySelector('.button').addEventListener('click', pairs)
